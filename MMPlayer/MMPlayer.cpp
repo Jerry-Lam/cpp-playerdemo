40bf2a0956e69780f33caaedf4de1ff2eb4d4fae
jerryLam <a17512049507@gmail.com>
2022-02-14 16:57:29 +0800

    第三次提交

--- MMPlayer/MMPlayer.cpp
+++ MMPlayer/MMPlayer.cpp
@@ -6,13 +6,24 @@ void threadFunction(int i) {
     printf("Thread Function\n%d", i);
 }
 
+void threadFun(int ThreadIndex) {
+    for (int i = 0; i < 1000; i++) {
+        printf("Thread: %d, index: %d\n", ThreadIndex, i);
+    }
+}
+
 int main()
 {
-    std::thread t(threadFunction, 10);
-    t.join();
+    // std::thread t(threadFunction, 10);
+    // t.join();
     // t.detach();
 
     // t.joinable();
 
+    std::thread t1(threadFun, 1);
+    std::thread t2(threadFun, 2);
+
+    t1.join();
+    t2.join();
     return EXIT_SUCCESS;
 }
